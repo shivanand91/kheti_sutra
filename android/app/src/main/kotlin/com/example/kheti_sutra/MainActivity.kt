0b3c313a0fb41f0c8ae6d3951c6aa2e2685da091
Shivanand Kumar <132545822+shivanand91@users.noreply.github.com>
2025-04-23 20:49:38 +0530

    project setup

---
+++ android/app/src/main/kotlin/com/example/kheti_sutra/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.kheti_sutra
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:kheti_sutra/ui/splash/splash_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Kheti Sutra',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.orange),
+        useMaterial3: true,
+      ),
+      home: SplashScreen()
+    );
+  }
+}
