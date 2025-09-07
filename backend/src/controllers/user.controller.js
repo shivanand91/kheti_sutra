f0a98176fee15b8d6f55d2ce6a8d15b4cc45e4b0
Aksquare <akashkush6272@gmail.com>
2025-09-07 14:10:41 +0530

    connect backed to frontend data exchange

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,90 +1,103 @@
-import User from "../models/user.model.js"
-import bcryptjs from "bcryptjs"
-import { genToken } from "../utiils/token.js"
+import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { genToken } from "../utiils/token.js";
 
 const signUp = async (req, res) => {
-    try {
-        const { name, email, password } = req.body
-        
-        if (!name || !email || !password) {
-            return res.status(400).json({ message: "All credentials required!" })
-        }
+   try {
+      const { name, email, password } = req.body;
+      console.log(name, email, password);
 
-        const existingUser = await User.findOne({ email })
-        
-        if (existingUser) {
-            return res.status(400).json({message: "User already exist!"})
-        }
+      if (!name || !email || !password) {
+         return res.status(400).json({ message: "All credentials required!" });
+      }
 
-        const hashPassword = await bcryptjs.hash(password, 10)
+      const existingUser = await User.findOne({ email });
 
-        const token = await genToken(user._id)
-        res.cookie("token", token)
+      if (existingUser) {
+         return res.status(400).json({ message: "User already exist!" });
+      }
 
-        const user = await User.create({
-            name,
-            email,
-            password: hashPassword
-        })
+      const hashPassword = await bcryptjs.hash(password, 10);
 
-        return res.status(200).json({message: "User sign up successfully!", user})
+      const user = await User.create({
+         name,
+         email,
+         password: hashPassword,
+      });
+      const token = await genToken(user._id);
+      res.cookie("token", token);
 
-    } catch (error) {
-        console.log("Error in sign up");        
-    }
-} 
+      console.log(user);
+      return res
+         .status(200)
+         .json({
+            message: "User sign up successfully!",
+            user: user,
+            success: true,
+         });
+   } catch (error) {
+      console.log(error);
+      console.log("Error in sign up");
+   }
+};
 
 const logIn = async (req, res) => {
-    try {
-        const { email, password } = req.body
-        
-        if (!email || !password) {
-            return res.status(400).json({ message: "All credentials required!" })
-        }
+   try {
+      const { email, password } = req.body;
 
-        const existingUser = await User.findOne({ email })
-        
-        if (!existingUser) {
-            return res.status(400).json({message: "User doesn't exist!"})
-        }
+      if (!email || !password) {
+         return res.status(400).json({ message: "All credentials required!" });
+      }
 
-        const isMatch = await bcryptjs.compare(password, existingUser.password)
+      const existingUser = await User.findOne({ email });
 
-        if (!isMatch) {
-            return res.status(400).json({message: "Invalid credentials"})
-        }
-        console.log("user logged in");
-        
-        return res.status(200).json({message: "User login successfully!"})
+      if (!existingUser) {
+         return res.status(400).json({ message: "User doesn't exist!" });
+      }
 
-    } catch (error) {
-        console.log("Error in Login");        
-    }
-} 
+      const isMatch = await bcryptjs.compare(password, existingUser.password);
+
+      if (!isMatch) {
+         return res.status(400).json({ message: "Invalid credentials" });
+      }
+      console.log("user logged in");
+
+      return res
+         .status(200)
+         .json({ message: "User login successfully!", user: existingUser });
+   } catch (error) {
+      console.log("Error in Login");
+   }
+};
 
 const getDetails = async (req, res) => {
-    try {
-        // Assuming email is provided in the request body.
-        const { email } = req.body;
-        if (!email) {
-            return res.status(400).json({ message: "Email required to fetch details!" });
-        }
-        
-        // Find the user by email and exclude the password field from the returned data.
-        const user = await User.findOne({ email }).select('-password');
-        if (!user) {
-            return res.status(404).json({ message: "User not found!" });
-        }
-        
-        return res.status(200).json({
-            message: "User details fetched successfully!",
-            user
-        });
-    } catch (error) {
-        console.log("Error while fetching the details of the user");
-        return res.status(500).json({ message: "Server error" });
-    }
-}
+   try {
+      // Assuming email is provided in the request body.
+      const { email } = req.query;
 
+      if (!email) {
+         return res
+            .status(400)
+            .json({ message: "Email required to fetch details!" });
+      }
 
-export {signUp, logIn, getDetails}
+      const user = await User.findOne({ email }).select("-password");
+
+      if (!user) {
+         return res
+            .status(404)
+            .json({ message: "User not found!", success: false });
+      }
+
+      return res.status(200).json({
+         message: "User details fetched successfully!",
+         user: user,
+         success: true,
+      });
+   } catch (error) {
+      console.log("Error while fetching the details of the user");
+      return res.status(500).json({ message: "Server error" });
+   }
+};
+
+export { signUp, logIn, getDetails };
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,14 +6,14 @@ const Home = () => {
 
   const [email, setEmail] = useState('')
   const [data, setData] = useState({})
-  
+  console.log(data)
   const handleSubmit = async(e) => {
     try {
       e.preventDefault()
-      const response = await axios.get("http://localhost:8000/api/user/getdetails", { email })
-      setData(response.data)
+      const response = await axios.get("http://localhost:8000/api/user/getdetails", { params :{email} })
+      setData(response.data.user)
     } catch (error) {
-      console.log("error while fetching the data");      
+      console.log(error,"error while fetching the data");      
     }
   }
   
@@ -21,7 +21,12 @@ const Home = () => {
     <div className=''>
       <input type="email" placeholder='Enter your email' value={email} onChange={(e) => setEmail(e.target.value)} />
       <button onClick={handleSubmit}>Get Details</button>
+      <br />
+      <br />
       <p>{ data.name }</p>
+      <p>{ data.email }</p>
+      <p>{ data.createdAt }</p>
+      <p>{ data.updatedAt }</p>
     </div>
   )
 }
